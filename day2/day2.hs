77f6f572cf7b5abfcd97e0a3ac37858cff38f3e5
totallyhuman <bsumantb@gmail.com>
2021-12-02 19:05:23 -0500

    Remove compiled code
    Also touch up day 2 code 'cause I can't stop myself.

--- day2/day2.hs
+++ day2/day2.hs
@@ -1,20 +1,20 @@
 part1f [] pos dep = pos * dep
 part1f (com : coms) pos dep
-    | com0 == 'f' = part1f coms (pos + x) dep
-    | com0 == 'u' = part1f coms pos (dep - x)
-    | com0 == 'd' = part1f coms pos (dep + x)
-    where com0 = head com
-          x    = read $ last $ words $ com
+    | dir == "forward" = part1f coms (pos + x) dep
+    | dir == "up"      = part1f coms pos (dep - x)
+    | dir == "down"    = part1f coms pos (dep + x)
+    where dir = head $ words com
+          x   = read $ last $ words com
 
 part1 list = part1f list 0 0
 
 part2f [] pos dep aim = pos * dep
 part2f (com : coms) pos dep aim
-    | com0 == 'f' = part2f coms (pos + x) (dep + aim * x) aim
-    | com0 == 'u' = part2f coms pos dep (aim - x)
-    | com0 == 'd' = part2f coms pos dep (aim + x)
-    where com0 = head com
-          x    = read $ last $ words $ com
+    | dir == "forward" = part2f coms (pos + x) (dep + aim * x) aim
+    | dir == "up"      = part2f coms pos dep (aim - x)
+    | dir == "down"    = part2f coms pos dep (aim + x)
+    where dir = head $ words com
+          x   = read $ last $ words com
 
 part2 list = part2f list 0 0 0
 
