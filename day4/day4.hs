91d60dd40d11e2a1a9ea30bad4fa398e925b1cdc
totallyhuman <bsumantb@gmail.com>
2021-12-04 17:25:24 -0500

    Clean up day 4 even more Can't really figure out how to rework the logic so...

--- day4/day4.hs
+++ day4/day4.hs
@@ -1,17 +1,19 @@
 import Data.List
-import Data.Ord
 
-isBingo nums board = any (== 5) [length (intersect nums l) | l <- board ++ transpose board]
-winners nums boards = [head [(b, n) | n <- drop 5 $ inits nums, isBingo n b] | b <- boards]
+isBingo nums board = 5 `elem` [length (nums `intersect` l) | l <- board ++ transpose board]
+
+winners nums boards = sortOn (length . snd) [head [(b, n) | n <- drop 5 $ inits nums, isBingo n b] | b <- boards]
 
 score (board, nums) = sum [i | r <- board, i <- r, i `notElem` nums] * last nums
 
-part1 nums boards = score $ minimumBy (comparing (length . snd)) $ winners nums boards
-part2 nums boards = score $ maximumBy (comparing (length . snd)) $ winners nums boards
+part1 nums boards = score $ head $ winners nums boards
 
-parseBoards []         = []
+part2 nums boards = score $ last $ winners nums boards
+
+parseBoards [] = []
 parseBoards (_ : list) = [map read $ words r | r <- board] : parseBoards boards
-                             where (board, boards) = span (/= "") list
+  where
+    (board, boards) = span (/= "") list
 
 main = do
     input <- lines <$> readFile "input.txt"
