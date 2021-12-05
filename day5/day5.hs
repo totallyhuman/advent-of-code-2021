d9a0a7f2f64a21bfba104df12c8e589e7f08495b
totallyhuman <bsumantb@gmail.com>
2021-12-05 13:32:04 -0500

    Clean up day 5 Prelude's lack of a "split list by list" function really annoys me...

--- day5/day5.hs
+++ day5/day5.hs
@@ -1,24 +1,36 @@
 import Data.Char (isDigit)
-import Data.List (group, sort)
+import Data.List (group, inits, isInfixOf, isSuffixOf, sort, (\\))
 
-isStraight :: [Int] -> Bool
-isStraight [x1, y1, x2, y2] = x1 == x2 || y1 == y2
+type Point = (Int, Int)
+type Line = (Point, Point)
 
-points :: [Int] -> [(Int, Int)]
-points [x1, y1, x2, y2]
-    | (x1, y1) /= (x2, y2) = (x1, y1) : points [x1 + signum (x2 - x1), y1 + signum (y2 - y1), x2, y2]
-    | otherwise = [(x1, y1)]
+isAxisAligned :: Line -> Bool
+isAxisAligned ((x1, y1), (x2, y2)) = x1 == x2 || y1 == y2
 
-part1 :: [[Int]] -> Int
-part1 = length . filter ((>= 2) . length) . group . sort . concatMap points . filter isStraight
+points :: Line -> [Point]
+points ((x1, y1), (x2, y2)) = zip [x1, xd .. x2] [y1, yd .. y2]
+    where xd = x1 + signum (x2 - x1)
+          yd = y1 + signum (y2 - y1)
 
-part2 :: [[Int]] -> Int
+
+part1 :: [Line] -> Int
+part1 = length . filter ((>= 2) . length) . group . sort . concatMap points . filter isAxisAligned
+
+
+part2 :: [Line] -> Int
 part2 = length . filter ((>= 2) . length) . group . sort . concatMap points
 
-parseLine :: String -> [Int]
-parseLine "" = []
-parseLine line = read num : parseLine (dropWhile (not . isDigit) nums)
-    where (num, nums) = span isDigit line
+
+split :: Eq a => [a] -> [a] -> [[a]]
+split d s
+    | d `isInfixOf` s = (x \\ d) : split d (s \\ x)
+    | otherwise       = [s]
+    where x = head $ dropWhile (not . isSuffixOf d) $ inits s
+
+parseLine :: String -> Line
+parseLine line = ((x1, y1), (x2, y2))
+    where [[x1, y1], [x2, y2]] = map (map read . split ",") $ split " -> " line
+
 
 main = do
   input <- map parseLine . lines <$> readFile "input.txt"
