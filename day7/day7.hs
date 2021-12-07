a51f4ffc5a3c806834b8d6f020545fab940dd63b
totallyhuman <bsumantb@gmail.com>
2021-12-07 00:18:35 -0500

    Add day 7 These are getting suspiciously easy... Maybe it's a setup for a bombshell of a Tuesday.

---
+++ day7/day7.hs
@@ -0,0 +1,14 @@
+part1 :: [Int] -> Int
+part1 l = minimum [sum [abs $ j - i | j <- l] | i <- [minimum l .. maximum l]]
+
+part2 :: [Int] -> Int
+part2 l = minimum [sum [f $ abs $ j - i | j <- l]| i <- [minimum l .. maximum l]]
+    where f x = x * (x + 1) `div` 2
+
+main = do
+    input <- readFile "input.txt"
+
+    let list = read $ "[" ++ input ++ "]"
+
+    print $ part1 list
+    print $ part2 list
